4129bf2a24525e74cb863800577b9f92699fb3b5
Createworld04 <94844308+Createworld04@users.noreply.github.com>
2022-04-21 14:50:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -580,8 +580,8 @@ async def account_login(bot: Client, m: Message):
                 rout = f"https://www.toprankers.com/?route=common/ajax&mod=liveclasses&ack=getcustompolicysignedcookiecdn&stream=https%3A%2F%2Fsignedsec.toprankers.com%2Flivehttporigin%2F{y[56:-14]}%2Fmaster.m3u8"
                 getstatusoutput(f'curl "{rout}" -c "cookie.txt"')
                 cook = "cookie.txt"
-                print (rout)
-                print(url)
+                # print (rout)
+                # print(url)
             elif raw_text0 in "sure60":
                 y= url.replace("/", "%2F")
                 rout = f"https://onlinetest.sure60.com/?route=common/ajax&mod=liveclasses&ack=getcustompolicysignedcookiecdn&stream=https%3A%2F%2Fvodcdn.sure60.com%2Flivehttporigin%2F{y[49:-14]}%2Fmaster.m3u8"
@@ -594,7 +594,7 @@ async def account_login(bot: Client, m: Message):
                 cook = "cookie.txt"
 	      elif raw_text0 in "anytimelearning":
 		y= url.replace("/", "%2F")
-		rout = f"https://live.anytimelearning.in/?route=common/ajax&mod=liveclasses&ack=getcustompolicysignedcookiecdn&stream=https%3A%2F%2Fvodcdn.anytimelearning.in%2F{y[44:-14]}%2Fmaster.m3u8
+		rout = f"https://live.anytimelearning.in/?route=common/ajax&mod=liveclasses&ack=getcustompolicysignedcookiecdn&stream=https%3A%2F%2Fvodcdn.anytimelearning.in%2F{y[44:-14]}%2Fmaster.m3u8"
 		getstatusoutput(f'curl "{rout}" -c "cookie.txt"')
 		cook = "cookie.txt"
             Show = f"**Downloading:-**\n\n**Name :-** `{name}`\n\n**Url :-** `{url}`"
